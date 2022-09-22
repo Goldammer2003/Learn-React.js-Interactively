51e791b0c0594f42f1bc836b57846ff533d43650
Manuel Alexander Goldammer <mangobo@hotmail.de>
2022-09-22 00:47:33 +0000

    Test

--- .learn/resets/01.3-building-a-layout/app.jsx
+++ .learn/resets/01.3-building-a-layout/app.jsx
@@ -12,21 +12,12 @@ const data = {
 	},
 };
 
-//let content = <img src={data.image} />;
+let content = (
+		<img src={data.image} />
+);
 /**
  * define the variable 'content' here and fill it with the
  * needed code to render the bootstrap card
  **/
-let content = (
-	<div class="card m-5">
-		<img class="card-img-top" src= [data.image"] alt="Card image cap" />
-		<div class="card-body">
-			<h5 class="card-title"> [data.card-Title]Card title</h5>
-			<p class="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
-			<a href="data.button.url" class="btn btn-primary"> {data.}
-				Go somewhere
-			</a>
-		</div>
-	</div>
-);
+
 ReactDOM.render(content, document.querySelector("#myDiv"));
